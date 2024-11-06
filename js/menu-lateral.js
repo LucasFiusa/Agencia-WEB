19fcf47525a4f3adf784d874ee97f5a17a5d8ed3
LucasFiusa <lucasfiusa100@gmail.com>
2024-11-05 22:31:53 -0300

    finalização

--- js/menu-lateral.js
+++ js/menu-lateral.js
@@ -1,16 +1,28 @@
-
 let btnMenu = document.getElementById('menu_icon');
 let menu = document.getElementById('menu_lateral');
 let overlay = document.getElementById('overlay_menu');
+let body = document.body;
 
-btnMenu.addEventListener('click', ()=>{
-    menu.classList.add('abrir-menu')
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu');
+    body.classList.add('no-scroll');
+});
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu');
+    body.classList.remove('no-scroll');
+});
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu');
+    body.classList.remove('no-scroll');
+});
+
+// SCROLL
+
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('#header')
+    header.classList.toggle('rolagem', window.scrollY > 0)
 })
 
-menu.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
-
-overlay.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
+// FIM SCROLL
